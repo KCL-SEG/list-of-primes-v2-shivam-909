373645837e16ad5c1a95adfe646c14cda5b9eef9
shivam <86538330+shivam-909@users.noreply.github.com>
2022-10-22 18:00:13 +0100

    primes: Add prime function

--- primes.py
+++ primes.py
@@ -1,6 +1,23 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+
+
+
+import math
 def primes(number_of_primes):
     list = []
+
+    i = 1
+    while len(list) != number_of_primes:
+        if is_prime(i):
+            list.append(i)
+        i = i + 1
     return list
+
+
+def is_prime(n):
+    for i in range(1, int(math.sqrt(n))+1):
+        if (n % i) == 0:
+            return False
+    return True
